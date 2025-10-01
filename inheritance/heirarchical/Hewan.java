b1d6329f06446361bda20b8888d5a8a9e6df7c69
F1RLY <firlyalamsudrajat@gmail.com>
2025-10-01 08:29:49 +0700

    !

---
+++ inheritance/heirarchical/Hewan.java
@@ -0,0 +1,10 @@
+package inheritance.heirarchical;
+
+public class Hewan {
+    public int usia;
+    public String nama;
+
+    public void makan(){
+        System.out.println("Hewan dengan nama "+ this.nama +" dengan usia " +this.usia+" Sedang makan");
+    }
+}

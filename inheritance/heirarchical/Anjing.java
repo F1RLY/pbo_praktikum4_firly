b19c5bf7cc8da7baac8c9f078eae815070ea3408
F1RLY <firlyalamsudrajat@gmail.com>
2025-10-01 08:35:38 +0700

    !

---
+++ inheritance/heirarchical/Anjing.java
@@ -0,0 +1,8 @@
+package inheritance.heirarchical;
+
+public class Anjing extends Hewan{
+    public void menggonggong(){
+        System.out.println("Anjing dengan nama " + this.nama +"dan usia " + this.usia + "Menggonggong "  );
+    }
+
+}
--- inheritance/heirarchical/Kucing.java
+++ inheritance/heirarchical/Kucing.java
@@ -1,5 +1,7 @@
 package inheritance.heirarchical;
 
-public class Kucing {
-    
+public class Kucing extends Hewan {
+    public void mengeong(){
+        System.out.println("Kucing bernama "+ this.nama+ " Mengeong" );
+    }    
 }
